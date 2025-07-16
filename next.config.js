0fdda12d5d95cb41b5a3d70e019a8f08b1280c53
Spyros Gavriil <spyrosgavriil01@gmail.com>
2025-07-16 23:16:04 +0300

    Vercel fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -11,8 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
-}
+  basePath: "",
+  assetPrefix: "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
